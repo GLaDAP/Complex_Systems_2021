52ded176072dfbeb635f88677951c73af0fdc5f9
GLaDAP <GLaDAP@users.noreply.github.com>
2021-06-17 15:15:11 +0200

    Merged firefighter class

--- forest_fire/firefighter.py
+++ forest_fire/firefighter.py
@@ -1,50 +1,46 @@
-from mesa import Agent
+"""
+GROUP:       CSS_18
+DATE:        17-06-2021
+AUTHOR(S):   Sam Kuilboer
+DESCRIPTION: FireFighter class with functions to extinguish fires. 
+             This class inherits the walker class.
+"""
+from Walker import Walker
 
-class FireFighter(Agent):
+class FireFighter(Walker):
     """
-    A FireFighter agent.
+    FireFighter class able of extinguishing fires.
 
     Attributes:
-        x, y: Grid coordinates
-        condition: Can be 'inactive', 'active'
-        unique_id: (x,y) tuple
+        unique_id: int
+        pos: tuple(x, y)
+        model: grid
+        moore: boolean
+        extg_strength: int within range [0, 100]
     """
 
-    def __init__(self, pos, model):
-        """
-        Create a firefighter tree.
-        Args:
-            pos: The firefighter's coordinates on the grid.
-            model: standard model reference for agent.
-        """
-        super().__init__(pos, model)
-        self.name = "FireFighter"
-        self.pos = pos
+    def __init__(self, unique_id, pos, model, moore, extg_strength):
+        super().__init__(unique_id, pos, model, moore=moore)
+        self.extg_strength = extg_strength
+        self.fires_extg = 0
 
     def step(self):
         """
-        If the firefighter is inactive it looks in a range of 3 for a fire.
+        If the firefighter is near a fire it will try to extinguish it,
+        if not, it will move randomly.
         """
-        neighbors = self.random.shuffle(self.model.grid.neightbor_iter(self.pos))
-        exth = False
-        for neighbor in neighbors:
-            if neighbor.name == 'Tree':
-                if neighbor.condition == 'On Fire':
-                    neighbor.condition = 'Fine'
-                    exth = True
-                    break
-        if exth == False:
-            self.move()
-
-    def move(self):
-        """
-        If the FireFighter does not extinguish a fire it is able to move to a next spot.
-        """
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=False,
-            include_center=False
+        neighbors = self.model.grid.get_neighbors(
+            pos = self.pos,
+            moore = False,
+            include_center = False,
+            radius = 1
         )
+        neighbouring_trees = [agent for agent in neighbors if isinstance(agent, CLASS>TREE)]
+        if any(neighbouring_trees.condition == 'On fire'):
+            slice_nb = [tree for tree in neighbouring_trees if tree.condition == 'On fire']
+            burning_tree = self.random.choice(slice_nb)
+            burning_tree.condition = 'Fine'
+            self.fires_extg += 1
+        else:
+            self.random_move()
 
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
