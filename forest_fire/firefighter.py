c0535899e832ff145d2a82b906fe7713f0b5f0d5
Jorrim Prins <jorrim.prins@hotmail.com>
2021-06-24 13:31:02 +0200

    Change standard parameters to percolating setting and change output name of report

--- forest_fire/firefighter.py
+++ forest_fire/firefighter.py
@@ -51,8 +51,10 @@ class FireFighter(Walker):
             self.move_towards_closest_fire()
         elif self.strategy == 'biggest':
             self.move_towards_biggest_fire()
-        elif self.strategy == 'call_plance':
+        elif self.strategy == 'call_plane':
             self.move_towards_biggest_fire()
+        elif self.strategy == 'earliest':
+            self.move_towards_hp_fire()
         else:
             self.random_move()
 
