7f9096109b7556a941a4598da9cf6f3c2c4a5d8a
S Kuilboer <s.kuilboer@gmail.com>
2021-06-24 14:20:04 +0200

    LATEST COMMIT FOR DAVID!

--- run_model.py
+++ run_model.py
@@ -7,10 +7,10 @@ from forest_fire.model import ForestFire
 import multiprocessing
 
 def calculate_model(config_tuple):
-    strategy, max_steps, n_fighters,ext_strength, CONFIG = config_tuple
+    strategy, max_steps, n_fighters,ext_strength, CONFIG, i = config_tuple
     model = ForestFire(
-        height = 128,
-        width = 128,
+        height = 100,
+        width = 100,
         density_trees = CONFIG['model']['density_trees'],
         max_burn_rate = CONFIG['model']['max_burn_rate'],
         ignition_prob = CONFIG['model']['ignition_prob'],
@@ -24,10 +24,12 @@ def calculate_model(config_tuple):
     model_reporting_dicts = []
     while max_steps > 0:
         statistics = model.step()
+        if statistics['On fire'] == 0:
+            break
         model_reporting_dicts.append(statistics)
         max_steps -=1
     statistics_df = pd.DataFrame(model_reporting_dicts)
-    statistics_df.to_csv(f'statistics_{strategy}_nfighters_{n_fighters}_ext_strength_{ext_strength}.csv')
+    statistics_df.to_csv(f'data/new_data/statistics_{strategy}_nfighters_{n_fighters}_ext_strength_{ext_strength}_{i}.csv')
 
     return 0
 
@@ -40,16 +42,18 @@ if __name__ == "__main__":
     configuration_list = []
     # 30 simulations
     for n_fighters in [100, 300, 500, 700, 900]:
-        for ext_strength in [4,10]:
-            configuration_list.append(('random', max_steps, n_fighters, ext_strength, CONFIG))
-            configuration_list.append(('closest', max_steps, n_fighters, ext_strength, CONFIG))
-            configuration_list.append(('biggest', max_steps, n_fighters, ext_strength, CONFIG))
+        for ext_strength in [10]:
+            for i in range(10):
+                configuration_list.append(('random', max_steps, n_fighters, ext_strength, CONFIG, i))
+                configuration_list.append(('closest', max_steps, n_fighters, ext_strength, CONFIG, i))
+                configuration_list.append(('biggest', max_steps, n_fighters, ext_strength, CONFIG, i))
 
     # 12 simulations
     for ext_strength in [2, 4, 6, 8]:
-        configuration_list.append(('random', max_steps, 100, ext_strength, CONFIG))
-        configuration_list.append(('closest', max_steps, 100, ext_strength, CONFIG))
-        configuration_list.append(('biggest', max_steps, 100, ext_strength, CONFIG))
+        for i in range(10):
+            configuration_list.append(('random', max_steps, 100, ext_strength, CONFIG, i))
+            configuration_list.append(('closest', max_steps, 100, ext_strength, CONFIG, i))
+            configuration_list.append(('biggest', max_steps, 100, ext_strength, CONFIG, i))
 
     with multiprocessing.Pool() as pool:
         outputs = pool.map(calculate_model, configuration_list)
